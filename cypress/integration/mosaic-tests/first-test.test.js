ce60a2a01de1529738dc02867754e43c6d38ab06
Allie <alliecaton3@gmail.com>
2021-07-13 14:12:57 -0400

    checks sorting of ascending on click then descending on second click

--- cypress/integration/mosaic-tests/first-test.test.js
+++ cypress/integration/mosaic-tests/first-test.test.js
@@ -5,11 +5,23 @@ describe('mosaic grid application', () => {
 		cy.visit('https://data-grid-qa-challenge-prod.herokuapp.com/')
 	})
 
-	it('displays table', () => {
-		cy.get('table > tbody > tr > :nth-child(1)').then(A => {
-			const names = A.toArray()
-			cy.log(names)
+	it('sorts in ascending', () => {
+		// count for the loop
+		// get headers and map over them
+		// on click o feach header
+		// get nth child of count
+		cy.get('table').get('thead > tr > :nth-child(1)').click()
+		cy.get('table > tbody > tr > :nth-child(1)').then($col => {
+			const names = $col.toArray().map($el => $el.innerText)
 			expect(names).to.be.sorted({ ascending: true })
+		})
+	})
+
+	it('sorts in descending on second click', () => {
+		cy.get('table').get('thead > tr > :nth-child(1)').click().click()
+		cy.get('table > tbody > tr > :nth-child(1)').then($col => {
+			const names = $col.toArray().map($el => $el.innerText)
+			expect(names).to.be.sorted({ descending: true })
 		})
 	})
 })
