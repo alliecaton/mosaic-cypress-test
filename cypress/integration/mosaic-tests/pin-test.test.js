824325e607fd02e62bffabd20a20c0cd40886392
Allie <alliecaton3@gmail.com>
2021-07-14 15:30:41 -0400

    checks pinning one at a time

--- cypress/integration/mosaic-tests/pin-test.test.js
+++ cypress/integration/mosaic-tests/pin-test.test.js
@@ -6,24 +6,34 @@ describe('pin columns to left grid', () => {
 		cy.wait(500)
 	})
 
-	for (let i = 1; i <= 5; i++) {
-		it('gets column', () => {
-			const originalHeaders = []
-			cy.get('table > thead > tr').then($col => {
-				originalHeaders.push($col.toArray()[0].childNodes)
+	it('gets column', () => {
+		// let originalHeaders = []
+		cy.get('table > thead > tr').within($header => {
+			cy.get('th').then($group => {
+				cy.get($group).each(($th, $index) => {
+					cy.get($th).click({ metaKey: true })
+					// .contains($group[$index - 1].innerText)
+					cy.get($group[0])
+						.should('contain', $group[$index].innerText) // checks that column properly moved
+						.should('have.attr', 'style')
+						.and('contain', 'background: yellow') // checks color turned yellow
+					cy.get($group[0]).click({ metaKey: true }) // unpins
+				})
 			})
-			cy.get(`table > thead > tr > :nth-child(${i})`)
-				.click({ metaKey: true })
-				.should('have.attr', 'style')
-				.should('contain', 'background: yellow') // checks color change
 
-			// meta click and store inner text on that node
-			// fine node with bg color if exists
-			// check the next sibling to be the node you just clicked
-			// if no bg color yellow is found, check the first header and compare with clicked
-			// click a random header and splice array snapshot to compare with header
+			// for (let i = 1; i <= 5; i++) {
+			// }
 		})
-	}
+
+		// .then($col => {
+		// 	originalHeaders.push($col.toArray()[0].childNodes)
+		// })
+
+		// cy.get(`table > thead > tr > :nth-child(${i})`).click({ metaKey: true })
+
+		// .should('have.attr', 'style')
+		// .and('contain', 'background: yellow') // checks color change
+	})
 })
 
 //
