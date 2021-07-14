1e484d6ab780e7844681528a72bf5e3523432af9
Allie <alliecaton3@gmail.com>
2021-07-13 22:10:14 -0400

    for loop checks all tests at once

--- cypress/integration/mosaic-tests/sorting-tests.test.js
+++ cypress/integration/mosaic-tests/sorting-tests.test.js
@@ -3,51 +3,54 @@
 describe('mosaic grid application', () => {
 	beforeEach(() => {
 		cy.visit('https://data-grid-qa-challenge-prod.herokuapp.com/')
+		cy.wait(500)
 	})
 
-	it('sorts in ascending', () => {
-		for (let i = 1; i < 4; i++) {
-			cy.get('table')
-				.get(`thead > tr > :nth-child(${i})`)
+	// check clicking the same column once, twice, three times in a row
+	// const headerLength = document.querySelectorAll('td')
+	// console.log(headerLength.length)
+	for (let i = 1; i <= 5; i++) {
+		it('sorts in ascending', () => {
+			cy.get(`table > thead > tr > :nth-child(${i})`)
 				.click()
 				.contains('A-Z') // checks that it contains the sorted label
-			cy.get(`table > tbody > tr > :nth-child(${i}`).then($col => {
-				const names = $col
-					.toArray()
-					.map($el =>
-						parseInt($el.innerText)
-							? parseInt($el.innerText.replace(/,/g, ''))
+				.then($col => {
+					const names = $col.toArray().map($el =>
+						parseInt($el.innerText) // check if string is a number or not for proper sorting
+							? parseFloat($el.innerText.replace(/,/g, ''))
 							: $el.innerText
 					)
-				cy.log('names', names)
-				expect(names).to.be.sorted({ ascending: true })
+					expect(names).to.be.sorted({ ascending: true })
+				})
+		})
+
+		it('sorts in descending on second click', () => {
+			cy.get(`table > thead > tr > :nth-child(${i})`)
+				.click()
+				.contains('A-Z') // checks that it contains the sorted label
+				.then($col => {
+					const names = $col.toArray().map($el =>
+						parseInt($el.innerText) // check if string is a number or not for proper sorting
+							? parseFloat($el.innerText.replace(/,/g, ''))
+							: $el.innerText
+					)
+					expect(names).to.be.sorted({ descending: true })
+				})
+		})
+
+		// store the original order, don't want to assume it's sorted alphabetically by default, just in case
+		it('returns to original order on third click', () => {
+			const original = []
+			cy.get(`table > tbody > tr > :nth-child(${i}`).then($col => {
+				$col.toArray().map($el => original.push($el.innerText))
 			})
-		}
-	})
-
-	it('sorts in descending on second click', () => {
-		cy.get('table')
-			.get('thead > tr > :nth-child(1)')
-			.click()
-			.click()
-			.contains('Z-A')
-		cy.get('table > tbody > tr > :nth-child(1)').then($col => {
-			const names = $col.toArray().map($el => $el.innerText)
-			expect(names).to.be.sorted({ descending: true })
+			cy.log(original)
+			cy.get('table').get(`thead > tr > :nth-child(${i}`).click()
+			cy.get(`table > tbody > tr > :nth-child(${i})`).each(($el, index) => {
+				// iterate over each element and compare to original
+				expect($el.text()).to.equal(original[index]) // compare each element text to the original text at the same index
+			})
 		})
-	})
-
-	// store the original order, don't want to assume it's sorted alphabetically by default, just in case
-	it('returns to original order on third click', () => {
-		const original = []
-		cy.get('table > tbody > tr > :nth-child(1)').then($col => {
-			$col.toArray().map($el => original.push($el.innerText))
-		})
-		cy.log(original)
-		cy.get('table').get('thead > tr > :nth-child(1)').click().click().click()
-		cy.get('table > tbody > tr > :nth-child(1)').each(($el, i) => {
-			// iterate over each element
-			expect($el.text()).to.equal(original[i]) // compare each element text to the original text at the same index
-		})
-	})
+	}
+	// check clicking a column, then switching to a new column
 })
