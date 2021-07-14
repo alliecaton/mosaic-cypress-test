a583fc3099500d9887793b847d4b081fb6ffaabb
Allie <alliecaton3@gmail.com>
2021-07-14 11:45:14 -0400

    starting pin tests

--- cypress/integration/mosaic-tests/pin-test.test.js
+++ cypress/integration/mosaic-tests/pin-test.test.js
@@ -3,13 +3,24 @@
 describe('pin columns to left grid', () => {
 	beforeEach(() => {
 		cy.visit('https://data-grid-qa-challenge-prod.herokuapp.com/')
+		cy.wait(500)
 	})
 
-	it('gets column', () => {
-		cy.get('table > thead > tr').then($col => {
-			cy.log($col)
+	for (let i = 1; i <= 5; i++) {
+		it('gets column', () => {
+			cy.get('table > thead > tr').then($col => {
+				const originalHeaders = $col.toArray()[0].childNodes
+			})
+			cy.get(`table > thead > tr > :nth-child(${i})`).click()
+			originalHeaders[0].should('have.css', 'background-color', 'rgb(0, 0, 0)')
+
+			// meta click and store inner text on that node
+			// fine node with bg color if exists
+			// check the next sibling to be the node you just clicked
+			// if no bg color yellow is found, check the first header and compare with clicked
+			// click a random header and splice array snapshot to compare with header
 		})
-	})
+	}
 })
 
 //
--- cypress/integration/mosaic-tests/sorting-tests.test.js
+++ cypress/integration/mosaic-tests/sorting-tests.test.js
@@ -2,53 +2,53 @@
 
 describe('mosaic grid application', () => {
 	beforeEach(() => {
-		cy.visit('https://data-grid-qa-challenge-prod.herokuapp.com/')
 		cy.wait(500)
 	})
 
-	// check clicking the same column once, twice, three times in a row
-	// const headerLength = document.querySelectorAll('td')
-	// console.log(headerLength.length)
+	it('loads the page', () => {
+		cy.visit('https://data-grid-qa-challenge-prod.herokuapp.com/')
+	})
 
 	for (let i = 1; i <= 5; i++) {
-		it('sorts in ascending', () => {
+		it(`sorts column ${i} in ascending`, () => {
 			cy.get(`table > thead > tr > :nth-child(${i})`)
 				.click()
 				.contains('A-Z') // checks that it contains the sorted label
 				.then($col => {
 					const names = $col.toArray().map($el =>
 						parseInt($el.innerText) // check if string is a number or not for proper sorting
-							? parseFloat($el.innerText.replace(/,/g, ''))
+							? parseFloat($el.innerText.replace(/,/g, '')) // remove commas in numbers
 							: $el.innerText
 					)
-					expect(names).to.be.sorted({ ascending: true })
+					expect(names).to.be.sorted({ ascending: true }) // assertion
 				})
 		})
 
-		it('sorts in descending on second click', () => {
+		it(`sorts column ${i} in descending on second click`, () => {
 			cy.get(`table > thead > tr > :nth-child(${i})`)
 				.click()
-				.contains('A-Z') // checks that it contains the sorted label
+				.contains('Z-A') // checks that it contains the sorted label
 				.then($col => {
 					const names = $col.toArray().map($el =>
 						parseInt($el.innerText) // check if string is a number or not for proper sorting
-							? parseFloat($el.innerText.replace(/,/g, ''))
+							? parseFloat($el.innerText.replace(/,/g, '')) // remove commas in numbers
 							: $el.innerText
 					)
-					expect(names).to.be.sorted({ descending: true })
+					expect(names).to.be.sorted({ descending: true }) // assertion
 				})
 		})
+	}
 
-		// store the original order, don't want to assume it's sorted alphabetically by default, just in case
-		it('returns to original order on third click', () => {
+	for (let i = 1; i <= 5; i++) {
+		it(`column ${i} returns to original order on third click`, () => {
 			const original = []
-			cy.get(`table > tbody > tr > :nth-child(${i}`).then($col => {
+			cy.get('table > tbody > tr > :nth-child(1)').then($col => {
 				$col.toArray().map($el => original.push($el.innerText))
-			})
-			cy.log(original)
-			cy.get('table').get(`thead > tr > :nth-child(${i}`).click()
-			cy.get(`table > tbody > tr > :nth-child(${i})`).each(($el, index) => {
-				expect($el.innerText).to.equal(original[index]) // compare each element text to the original text at the same index
+			}) // stores what the original array was on load. don't want to assume original was sorted alphabetically
+
+			cy.get('table').get('thead > tr > :nth-child(1)').click().click().click()
+			cy.get('table > tbody > tr > :nth-child(1)').each(($el, index) => {
+				expect($el.innerText).to.equal(original[index].innerText) // compare each element text to the original text at the same index
 			})
 		})
 	}
